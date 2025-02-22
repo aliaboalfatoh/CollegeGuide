cbd9c1003a2f514705dd30fe32c7c475a0d1f98d
aliaboalfatoh <aliaboalfatoh8@gmail.com>
2025-02-22 07:27:32 -0800

    Add files via upload

---
+++ CollegeGuide/Controllers/UniversitiesController.cs
@@ -0,0 +1,36 @@
+﻿using CollegeGuide.Core.Entities;
+using CollegeGuide.Core.Repositories;
+using CollegeGuide.Core.Specifications;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CollegeGuide.Controllers
+{
+    public class UniversitiesController : APIBaseController
+    {
+        private readonly IGenericRepoitory<University> _UniversityRepo;
+
+        public UniversitiesController(IGenericRepoitory<University> UniversityRepo)
+        {
+            _UniversityRepo = UniversityRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<University>>> GetUniversity()
+        {
+            var Spec = new UniversityWithCollegeandFinancialaids();
+            var universities = await _UniversityRepo.GetAllAsync();
+            return Ok(universities);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<University>> GetUniversityById(int id)
+        {
+            var University = await _UniversityRepo.GetByIdAsync(id);
+            return Ok(University);
+        }
+
+    }
+}
